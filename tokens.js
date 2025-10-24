42aca4ea6b6b163a54b288c0827ae07b55c65f04
abodk108658 <28912aec34@gmail.com>
2025-10-25 01:34:48 +0300

    Create tokens.js

---
+++ tokens.js
@@ -0,0 +1,18 @@
+// status can be "online", "idle", "dnd", or "invisible" or "offline"
+export default [
+    {
+        channelId: "1420820092761014363",
+        serverId: "1323926281162588190",
+        token: process.env.token1,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "online",
+        },
+        selfMute: False,
+    },
+];
